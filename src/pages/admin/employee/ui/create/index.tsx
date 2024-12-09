0a5e3106b71b1c957442737e75368140744dbcf0
IskanderDev01 <githubturabaevi01@gmail.com>
2024-12-09 18:33:32 +0500

    employee create new design form

--- src/pages/admin/employee/ui/create/index.tsx
+++ src/pages/admin/employee/ui/create/index.tsx
@@ -7,9 +7,9 @@ import { IEmployeePost } from '@/entities/employee/model/types';
 import { useGetOrganizationQuery } from '@/entities/organization/api';
 import { useGetAllRolesQuery } from '@/entities/role/api';
 import { useGetAllSchedulesQuery } from '@/entities/schedule/api';
-import { FlexBox, useAppActions } from '@/shared';
+import { useAppActions } from '@/shared';
 import { mapToOptions } from '@/shared/lib/mapToOptions';
-import { Button, Form, Input, Select, Upload, message } from 'antd';
+import { Button, Col, Form, Input, Row, Select, Upload, message } from 'antd';
 import { useEffect, useMemo, useState } from 'react';
 import { UploadOutlined } from '@ant-design/icons';
 
@@ -73,114 +73,181 @@ const AdminCreateEmployeeForm = () => {
     useEffect(() => () => onCancel(), []);
 
     return (
-        <Form
-            form={form}
-            labelCol={{ span: 8 }}
-            wrapperCol={{ span: 8 }}
-            onFinish={onSubmit}
-            layout="vertical"
-        >
-            <Form.Item
-                label="Выберите изображение"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Пожалуйста, выберите изображение!',
-                    },
-                ]}
-            >
-                <Upload
-                    beforeUpload={() => false}
-                    maxCount={1}
-                    accept="image/*"
-                    onChange={handleUploadChange}
+        <div className="pt-10 flex justify-center items-center">
+            <div className="w-full max-w-4xl p-6 bg-white rounded-lg shadow-lg">
+                <h2 className="text-2xl font-semibold text-center mb-6">
+                    Создать сотрудника
+                </h2>
+                <Form
+                    form={form}
+                    onFinish={onSubmit}
+                    layout="vertical"
+                    className="space-y-4"
                 >
-                    <Button icon={<UploadOutlined />}>
-                        Выбрать изображение
-                    </Button>
-                </Upload>
-            </Form.Item>
-            <Form.Item<IEmployeePost>
-                name="first_name"
-                label="Имя"
-                rules={[
-                    { required: true, message: 'Пожалуйста, заполните поле!' },
-                ]}
-            >
-                <Input />
-            </Form.Item>
-            <Form.Item<IEmployeePost>
-                name="last_name"
-                label="Фамилия"
-                rules={[
-                    { required: true, message: 'Пожалуйста, заполните поле!' },
-                ]}
-            >
-                <Input />
-            </Form.Item>
-            <Form.Item<IEmployeePost>
-                name="position_id"
-                label="Должность"
-                rules={[
-                    { required: true, message: 'Пожалуйста, заполните поле!' },
-                ]}
-            >
-                <Select disabled={!roleOptions?.length} options={roleOptions} />
-            </Form.Item>
-            <Form.Item<IEmployeePost>
-                name="organization_id"
-                label="Организация"
-                rules={[
-                    { required: true, message: 'Пожалуйста, заполните поле!' },
-                ]}
-            >
-                <Select
-                    disabled={!organizationOptions?.length}
-                    options={organizationOptions}
-                />
-            </Form.Item>
-            <Form.Item<IEmployeePost>
-                name="branch_id"
-                label="Филиал"
-                rules={[
-                    { required: true, message: 'Пожалуйста, заполните поле!' },
-                ]}
-            >
-                <Select
-                    disabled={!branchOptions?.length}
-                    options={branchOptions}
-                />
-            </Form.Item>
-            <Form.Item<IEmployeePost>
-                name="schedule_id"
-                label="Рабочий график"
-                rules={[
-                    { required: true, message: 'Пожалуйста, заполните поле!' },
-                ]}
-            >
-                <Select
-                    disabled={!scheduleOptions?.length}
-                    options={scheduleOptions}
-                />
-            </Form.Item>
-            <Form.Item<IEmployeePost>
-                name="phone"
-                label="Телефон"
-                rules={[
-                    { required: true, message: 'Пожалуйста, заполните поле!' },
-                ]}
-            >
-                <Input />
-            </Form.Item>
-            <FlexBox>
-                <Button onClick={onCancel} type="default">
-                    Отмена
-                </Button>
-                <Button loading={loading} type="primary" htmlType="submit">
-                    Сохранить
-                </Button>
-            </FlexBox>
-        </Form>
+                    <Row gutter={16}>
+                        <Col xs={24} sm={12}>
+                            <Form.Item
+                                name="first_name"
+                                label="Имя"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Пожалуйста, заполните поле!',
+                                    },
+                                ]}
+                            >
+                                <Input placeholder="Введите имя" />
+                            </Form.Item>
+                        </Col>
+                        <Col xs={24} sm={12}>
+                            <Form.Item
+                                name="position_id"
+                                label="Должность"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message:
+                                            'Пожалуйста, выберите должность!',
+                                    },
+                                ]}
+                            >
+                                <Select
+                                    disabled={!roleOptions?.length}
+                                    options={roleOptions}
+                                    placeholder="Выберите должность"
+                                />
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    <Row gutter={16}>
+                        <Col xs={24} sm={12}>
+                            <Form.Item
+                                name="last_name"
+                                label="Фамилия"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Пожалуйста, заполните поле!',
+                                    },
+                                ]}
+                            >
+                                <Input placeholder="Введите фамилию" />
+                            </Form.Item>
+                        </Col>
+
+                        <Col xs={24} sm={12}>
+                            <Form.Item
+                                name="organization_id"
+                                label="Организация"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message:
+                                            'Пожалуйста, выберите организацию!',
+                                    },
+                                ]}
+                            >
+                                <Select
+                                    disabled={!organizationOptions?.length}
+                                    options={organizationOptions}
+                                    placeholder="Выберите организацию"
+                                />
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    <Row gutter={16}>
+                        <Col xs={24} sm={12}>
+                            <Form.Item
+                                name="phone"
+                                label="Телефон"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message:
+                                            'Пожалуйста, введите номер телефона!',
+                                    },
+                                ]}
+                            >
+                                <Input placeholder="Введите номер телефона" />
+                            </Form.Item>
+                        </Col>
+                        <Col xs={24} sm={12}>
+                            <Form.Item
+                                name="schedule_id"
+                                label="Рабочий график"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Пожалуйста, выберите график!',
+                                    },
+                                ]}
+                            >
+                                <Select
+                                    disabled={!scheduleOptions?.length}
+                                    options={scheduleOptions}
+                                    placeholder="Выберите график"
+                                />
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    <Row gutter={16}>
+                        <Col xs={24} sm={12}>
+                            <Form.Item
+                                label="Выберите изображение"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message:
+                                            'Пожалуйста, выберите изображение!',
+                                    },
+                                ]}
+                            >
+                                <Upload
+                                    beforeUpload={() => false}
+                                    maxCount={1}
+                                    accept="image/*"
+                                    onChange={handleUploadChange}
+                                >
+                                    <Button icon={<UploadOutlined />}>
+                                        Выбрать изображение
+                                    </Button>
+                                </Upload>
+                            </Form.Item>
+                        </Col>
+                        <Col xs={24} sm={12}>
+                            <Form.Item
+                                name="branch_id"
+                                label="Филиал"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Пожалуйста, выберите филиал!',
+                                    },
+                                ]}
+                            >
+                                <Select
+                                    disabled={!branchOptions?.length}
+                                    options={branchOptions}
+                                    placeholder="Выберите филиал"
+                                />
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    <div className="flex justify-end space-x-4">
+                        <Button onClick={onCancel} type="default">
+                            Отмена
+                        </Button>
+                        <Button
+                            loading={loading}
+                            type="primary"
+                            htmlType="submit"
+                        >
+                            Сохранить
+                        </Button>
+                    </div>
+                </Form>
+            </div>
+        </div>
     );
 };
 
